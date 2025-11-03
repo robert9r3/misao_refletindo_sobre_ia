ed3b97fbbf1ad6433b4055c9e5516e553b603825
robert9r3 <rvs128652@gmail.com>
2025-11-03 07:50:12 -0300

    .

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const caixaPrincipal = document.querySelector(".caixa-principal")
-const caixaPerguntas = document.querySelector(".caixa-perguntas")
-const caixaAlternativas = document.querySelector(".caixa-alternativas")
-const caixaResultado = document.querySelector("caixa-resultado")
-const textoResultado = document.querySelector("texto-resultado")
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+const caixaImagem = document.querySelector(".caixa-imagem");
 
 const perguntas = [
   {
@@ -17,65 +18,68 @@ const perguntas = [
             texto: "Isso é maravilhoso!",
             afirmacao: "afirmação"
         }
-    ]
-            
+    ],
+    imagem: "./img/Ética no Uso de IA em trabalhos.png"
   },
   {
     enunciado:
       "Com a descoberta desta tecnologia, chamada Inteligência Artificial (IA), uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
     alternativas: [
         {
-            texto:  "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-            afirmacao: "afirmação"
+      texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+      afirmacao: "afirmação"
         },
         {
-            texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-            afirmacao: "afirmação"
-      }
-    ]
+      texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+        afirmacao: "afirmação"
+        }
+    ],
+    imagem: "./img/IA e o Futuro do Trabalho.png"
   },
   {
     enunciado:
       "Após a elaboração do trabalho, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
     alternativas: [
-        {
-            texto:  "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-            afirmacao: "afirmação"
-        },
-        {
-            texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendo a importância de proteger os trabalhadores.",
-            afirmacao: "afirmação"
-
-        }
-    ]
+      {
+      texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+      afirmacao: "afirmação"
+      },
+      {
+      texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendo a importância de proteger os trabalhadores.",
+      afirmacao: "afirmação"
+      }
+    ],
+    imagem: "./img/IA na Educação - Debate e Perspectivas.png"
   },
   {
     enunciado:
       "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
     alternativas: [
-        {
-            texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-            afirmacao: "afirmação"
-        },
-        {
-            texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-            afirmacao: "afirmação"
-        }
-    ]
+      {
+      texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+      afirmacao: "afirmação"
+      },
+      {
+      texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+        afirmacao:"afirmação"
+      }
+      ],
+    imagem: "./img/Métodos de Pesquisa - IA vs tradicional.png"
   },
   {
     enunciado:
       "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda de uma IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz?",
     alternativas: [
-        {
-            texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-            afirmacao: "afirmação"
-        },
-        {
-            texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-            afirmacao: "afirmação"
-        }
+      {
+      texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+      afirmacao: "afirmação"
+      },
+      {
+      texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+        afirmacao:"afirmação"
+      }
     ],
+    imagem: "./img/.png"
   },
 ];
 
@@ -84,26 +88,31 @@ let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
 
-function mostraPergunta() {
+function mostraPergunta(){
   if (atual >= perguntas.length){
-  mostraResultado();
-return;
+    mostraResultado();
+  return
 }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+
+    caixaImagem.innerHTML = `<img src="${perguntaAtual.imagem}" alt="Ilustração da pergunta" class="imagem-pergunta">`;
+
     caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
 
-function mostraAlternativas() {
+
+
+
+function mostraAlternativas(){
     for (const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa))
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
-      }
-        
     }
+}
 
 function respostaSelecionada(opcaoSelecionada){
   const afirmacoes = opcaoSelecionada.afirmacoes;
@@ -112,5 +121,11 @@ function respostaSelecionada(opcaoSelecionada){
   mostraPergunta();
 }
 
+function mostraResultado(){
+  caixaPerguntas.textContent = "Em 2049...";
+  textoResultado.textContent = historiaFinal;
+  caixaAlternativas.textContent = "";
+caixaImagem.innerHTML = `<img src="./img/2049 - Futuro da IA.png" alt="O Futuro da IA em 2049" class="imagem-pergunta">`;
+}
 
-mostrapeguntas();
+mostraPergunta();
